582ee0f7f236e0b9d2c78c166e462b468065a786
Uriel Maldonado <blowitup2001@gmail.com>
2025-03-26 12:57:40 -0600

    Create automated test formatCurrency

---
+++ JS/LS12 - JS AMAZON PROJECT/tests/moneyTest.js
@@ -0,0 +1,26 @@
+import formatCurrency from "../js/utils/money.js";
+
+/* 
+    BASIC TEST
+    Just test if the code is working properly with simple data
+
+    EDGE TEST
+    Test with values that are tricky
+*/
+console.log('TEST SUIT - formatCurrency:'); //TITLE
+
+console.log('1.- Converts cents to dollars:');
+(formatCurrency(2095) === '20.95') ? console.log('PASSED') : 
+    console.log(`INCORRECT! :: ${formatCurrency(2095)}`);       //BASIC
+
+console.log('2.- Works with 0:');
+(formatCurrency(0) === '0.00') ? console.log('PASSED') : 
+    console.log(`INCORRECT! :: ${formatCurrency(0)}`);          //EDGE
+
+console.log('3.- Rounds up:');
+(formatCurrency(2000.5) === '20.01') ? console.log('PASSED') : 
+    console.log(`INCORRECT! :: ${formatCurrency(2000.5)}`);     //EDGE
+
+console.log('4.- Rounds down:');
+(formatCurrency(2000.4) === '20.00') ? console.log('PASSED') : 
+    console.log(`INCORRECT! :: ${formatCurrency(2000.4)}`);     //EDGE
