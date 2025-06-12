299c8c5cbcfd99c1c545ba766de33265e428407e
Uriel Maldonado <blowitup2001@gmail.com>
2025-06-12 13:29:00 -0600

    Fragments

---
+++ REACT/1-react-app/src/components/Card.tsx
@@ -0,0 +1,26 @@
+function Card() {
+  return (
+    <div className="card" style={{ width: 300 }}>
+      <div className="card-body">
+        <CardBody />
+      </div>
+    </div>
+  );
+}
+
+export function CardBody() {
+  return (
+    <>
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card’s content.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </>
+  );
+}
+
+export default Card;
