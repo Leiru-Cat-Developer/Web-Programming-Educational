75285f6f0163df445a81a2fd340d8508bea85e02
Uriel Maldonado <blowitup2001@gmail.com>
2025-06-12 15:14:26 -0600

    Gestion de Eventos

--- REACT/1-react-app/src/components/List.tsx
+++ REACT/1-react-app/src/components/List.tsx
@@ -1,13 +1,22 @@
+// import { MouseEvent } from "react";
+
 type Props = {
   data: string[];
 };
 
 function List({ data }: Props) {
+  const handleClick = (event: string) => {
+    console.log(event);
+  };
   return (
     <ul className="list-group">
       {/* Pasar el key es importante para que se asigne un identificador a cada dato del arreglo */}
       {data.map((elemento) => (
-        <li key={elemento} className="list-group-item">
+        <li
+          onClick={() => handleClick(elemento)}
+          key={elemento}
+          className="list-group-item"
+        >
           {elemento}
         </li>
       ))}
