923fcc3b29df75ffcbeecb720bb8f86bc836c591
Uriel Maldonado <blowitup2001@gmail.com>
2025-06-16 13:55:11 -0600

    Funciones como PROPS

--- REACT/1-react-app/src/App.tsx
+++ REACT/1-react-app/src/App.tsx
@@ -3,13 +3,25 @@ import List from "./components/List";
 
 function App() {
   const list = ["Knight", "Hornet", "Pure Vessel", "Final Bright"];
+
+  //Primera Funcion
+  const handleSelect1 = (elemento: string) => {
+    console.log("IMPRIMIENDO", elemento);
+  };
+
+  //Segunda Funcion
+  const handleSelect2 = (elemento: string) => {
+    console.log("MOSTRANDO", elemento);
+  };
+
   return (
     <Card>
       <CardBody
         title="CHILDREN"
         text="Children is used for item creation without repeat code"
       />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect1} />
+      <List data={list} onSelect={handleSelect2} />
     </Card>
   );
 }
--- REACT/1-react-app/src/components/List.tsx
+++ REACT/1-react-app/src/components/List.tsx
@@ -4,19 +4,21 @@ import { useState } from "react";
 
 type Props = {
   data: string[];
+  onSelect?: (elemento: string) => void;
 };
 
-function List({ data }: Props) {
+function List({ data, onSelect }: Props) {
   const [index, setIndex] = useState(1);
-  const handleClick = (i: number) => {
+  const handleClick = (i: number, elemento: string) => {
     setIndex(i);
+    onSelect?.(elemento); //Puede que la funcion se ocupe o no
   };
   return (
     <ul className="list-group">
       {/* Pasar el key es importante para que se asigne un identificador a cada dato del arreglo */}
       {data.map((elemento, i) => (
         <li
-          onClick={() => handleClick(i)}
+          onClick={() => handleClick(i, elemento)}
           key={elemento}
           className={`list-group-item ${index == i ? "active" : ""}`}
         >
